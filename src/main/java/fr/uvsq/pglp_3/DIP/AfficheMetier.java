20e75da0bd6ab810e4ab5223b399910300f1dc7b
uvsq21918311 <elkadi96hotmail.com>
2020-03-04 10:33:12 +0100

    Add slf4j

--- src/main/java/fr/uvsq/pglp_3/DIP/AfficheMetier.java
+++ src/main/java/fr/uvsq/pglp_3/DIP/AfficheMetier.java
@@ -2,19 +2,24 @@ package fr.uvsq.pglp_3.DIP;
 
 import java.time.LocalDateTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AfficheMetier implements AffichageMetier {
 	/**
 	 * fonction pour l'affichage du log au début.
 	 */
 	public void afficheDebut() {
-		System.out.println(LocalDateTime.now()
+		Logger logger = LoggerFactory.getLogger(AfficheMetier.class);
+	    logger.info(LocalDateTime.now()
 				+ " : Début de UneMethodeMetier");
 	}
 	/**
 	 * fonction pour l'affichage du log à la fin.
 	 */
 	public void afficheFin() {
-		System.out.println(LocalDateTime.now()
+		Logger logger = LoggerFactory.getLogger(AfficheMetier.class);
+	    logger.info(LocalDateTime.now()
 				+ " : Fin de UneMethodeMetier");
 	}
 }
